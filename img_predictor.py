8daad13f638c9eb71329f3636cbaed7d8ab3871f
AKinoshi <akinoshi@my.centennialcollege.ca>
2023-04-17 13:31:26 -0400

    fixed prediction

--- img_predictor.py
+++ img_predictor.py
@@ -1,17 +1,20 @@
 
 # Load requirments
 from tensorflow import keras
-from torchvision import transforms
+from PIL import Image
+import numpy as np
+
 
 class img_predictor:
     def __init__(self, ver="1"):
         # Parameters
         model_path = "./models/ver."+ver+"/model_inc.hdf5"
         self.model = keras.models.load_model(model_path)
+        self.img_size = (299,299)
 
     def predict(self, image):
         # Pre-process image
-        transform = transforms.Compose([
+        """transform = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
@@ -19,9 +22,27 @@ class img_predictor:
 
         p_image = transform(image)
         p_image = p_image.numpy()
+        """
+        p_image = Image.fromarray(image)
+        p_image = p_image.resize(self.img_size)
+
+        p_image = np.asarray(p_image)
+        p_image = p_image / 255
+        p_image = p_image.tolist()
 
         # Predict image
         pred = self.model.predict(p_image)
 
         ## TODO return prediction with meaningful json container?
         return pred
+
+"""
+Requirement:
+Use PIL.Image to open image file 
+Use numpy.asarray to convert image into numpy array
+Pass the converted numpy array to predict function
+
+or
+
+Pass numpy array of image to predict function
+"""
